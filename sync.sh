a52f9f3f3dfd63730599fe47b83c80e15c09ee28
Apsu <apsu@propter.net>
2026-01-13 15:59:31 +0000

    Sync data

--- sync.sh
+++ sync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export GITHUB_TOKEN="$(<github-pat.txt)"
+
 cd ~/workspace/cmini
 git add -A authors.json corpora.json likes.json links.json layouts cache
 git commit -m "Sync data"
